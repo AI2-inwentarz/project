6d8fff550cce7253536d71fede2586117d4fdf1c
WJakubW <79661196+WJakubW@users.noreply.github.com>
2023-01-11 20:35:33 +0100

    Więcej tekstu na głównej

--- frontend/src/views/Main.js
+++ frontend/src/views/Main.js
@@ -3,11 +3,21 @@ import { Blockquote } from '@mantine/core';
 export default function Main(){
     return(
         <div>
-            <Blockquote cite="– Właściciele portalu inwentarzor">
+            <Blockquote cite="– Właściciele portalu inwentarzu">
                  <b>Szanowni Państwo</b> <br/>
                 Serdecznie zapraszamy do odwiedzenia naszej witryny internetowej. <br />
                 Nasza strona oferuje szeroki zakres narzędzi i funkcji, które ułatwią Państwu zarządzanie swoim inwentarzem <hr />
                 Zapraszamy do zapoznania się z naszą ofertą i korzystania z naszej platformy! <hr />
+                <br/>
+
+
+
+                <br/>Jeśli jesteście zainteresowani optymalizacją procesów zarządzania inwentarzem, to jesteście we właściwym miejscu.
+                <br/>Nasza strona internetowa oferuje najnowocześniejszy system inwentarza, który pozwoli Państwu monitorować i zarządzać swoim inwentarzem w jednym miejscu. 
+                <br/>System inwentarza oferuje szeroki zakres funkcji, w tym zarządzanie magazynem, wykrywanie nadmiernych zapasów, zbieranie raportów i wiele innych.
+                <br/>Z naszym systemem inwentarza możesz śledzić swoje zasoby i wprowadzić własne reguły, aby zapewnić optymalne zarządzanie magazynem.
+                <br/>Nasz system inwentarza jest dostępny bezpłatnie, a dzięki intuicyjnemu interfejsowi użytkownika będziesz mógł łatwo i szybko go skonfigurować i w pełni wykorzystać. 
+                <br/>Nie czekaj dłużej! Skorzystaj z naszego systemu inwentarza, aby uprościć swoje procesy zarządzania magazynem
             </Blockquote>
         </div>
     )
