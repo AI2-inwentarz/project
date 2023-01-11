cc0ed952e71d685e223cf9c57bbfd6bb4a3938b8
WJakubW <79661196+WJakubW@users.noreply.github.com>
2023-01-11 19:39:02 +0100

    XSS Sanitization

--- backend/index.js
+++ backend/index.js
@@ -45,6 +45,9 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
+const {xss} = require("express-xss-sanitizer");
+app.use(xss());
+
 var apiRoute = require("./api_route.js");
 app.use("/api", apiRoute);
 
