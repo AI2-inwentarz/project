3f1767c7dd7ffdd4de5266ad0343dfca513ee3aa
jakubfalowski <kubafa99@gmail.com>
2023-01-12 11:31:09 +0100

    feat: items in room

--- frontend/src/views/RoomInfo.js
+++ frontend/src/views/RoomInfo.js
@@ -1,6 +1,34 @@
+import {useEffect, useState} from "react";
 import Items from "../components/Items";
 import { IItemsData } from '../exampleData/IItemsData';
+import { useParams } from 'react-router-dom';
+
+const json = localStorage.getItem("token")
+const item = JSON.parse(json)
+const jwt = item.value;
 
 export default function RoomInfo(){
-    return(<Items data={IItemsData}/>)
+
+    const { name } = useParams();
+    const [data, setData] = useState();
+
+    async function fetchData(room) {
+        await fetch(`http://${window.location.hostname}:9000/api/user/getDepartmentItems/${room}`, {
+            method: "GET",
+            headers: {
+                "content-type": "application/json; charset=UTF-8",
+                "authorization": `Bearer ${jwt}`
+            },
+            
+            })
+            .then(async res => {
+                const tokenObject = await res.json();
+                setData(tokenObject);
+            }) 
+    }
+
+    useEffect(()=>{
+        fetchData(name)
+    },[])
+    return(<>{data && <Items data={data}/>}</>)
 }
