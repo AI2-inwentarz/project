b051f0c9a2985f18146ef676be284fc415dddb5a
jakubfalowski <kubafa99@gmail.com>
2023-01-12 11:04:41 +0100

    feat: mailing

--- frontend/src/views/RequestAssigment.js
+++ frontend/src/views/RequestAssigment.js
@@ -11,6 +11,8 @@ export default function RequestAssigment(){
   const item = JSON.parse(json)
   const jwt = item.value;
 
+  console.log(data)
+
   async function fetchData() {
     await fetch(`http://${window.location.hostname}:9000/api/user/getContacts`, {
         method: "GET",
@@ -40,25 +42,22 @@ export default function RequestAssigment(){
               transform: 'rotate(45deg)',
             },
         }}>
-          { data && Object.values(data.departaments).map(item =>{
-            Object.values(data.users).map(user =>{
-              if(item.owner_id === user.id){
-                return(
-                  <Accordion.Item value={"item.name"} key={"item.name"}>
-                    <Accordion.Control>{"item.name"}</Accordion.Control>
+          { data && Object.values(data.depardments).map(item =>{
+            return Object.values(data.users).map(user =>(
+              item.owner_id === user.id &&
+                  <Accordion.Item value={item.name} key={item.name}>
+                    <Accordion.Control>{item.name}</Accordion.Control>
                     <Accordion.Panel>
-                      <Text weight={200}>Właściciel: {"user.name"} {"user.surname"} </Text><br />
-                      <Text weight={100}>{"user.job_title"}</Text>
-                      <Anchor href={`mailto:user.email`}target="_blank">
+                      <Text weight={200}>Właściciel: {user.firstname} {user.surname} </Text>
+                      <Text weight={100}>{user.job_title}</Text>
+                      <Anchor href={`mailto:${user.email}`}target="_blank">
                           <Button sx={{backgroundColor: "#ad8881", '&:hover': {backgroundColor:"#4d331f"}}}> Wyślij maila</Button>
                       </Anchor>
                       
                     </Accordion.Panel> 
                   </Accordion.Item>
-                )
-              }
             
-        })})}
+        ))})}
       </Accordion>
     </>
     )
