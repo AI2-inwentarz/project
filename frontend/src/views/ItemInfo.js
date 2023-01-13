dcb4b5cd0494dc9b263b718eb488b982f64cf734
jakubfalowski <kubafa99@gmail.com>
2023-01-13 11:44:06 +0100

    feat: item page

--- frontend/src/views/ItemInfo.js
+++ frontend/src/views/ItemInfo.js
@@ -1,12 +1,58 @@
 import { useParams } from 'react-router-dom';
+import { useEffect, useState } from 'react';
+import { Text, Paper } from '@mantine/core';
+import { concatSeries } from 'async';
 
-export function ItemInfo(){
+export default function ItemInfo(){
 
-const { name } = useParams();
+    const { name } = useParams();
+    const [data, setData] = useState();
 
-return(
-    <div>{name}</div>
-)
+    const json = localStorage.getItem("token")
+    const item = JSON.parse(json)
+    const jwt = item.value;
+
+    console.log(data)
+
+    async function fetchData(name) {
+        await fetch(`http://${window.location.hostname}:9000/api/user/items/${name}`, {
+            method: "GET",
+            headers: {
+                "content-type": "application/json; charset=UTF-8",
+                "authorization": `Bearer ${jwt}`
+            },
+        
+            
+            }).then(async res => {
+                const resObject = await res.json();
+                setData(resObject);
+        
+        })
+    }
+
+    useEffect(()=>{
+        fetchData(name);
+    },[])
+
+    if(data){
+        Object.values(data).map(item =>{
+            console.log(item)
+        })
+    }
+
+    return(
+        <>
+            {data && 
+
+                <Paper shadow="lg" radius="xl" p="xl" withBorder>
+                    <Text weight={500}>{data.name} #{data.inventory_tag}</Text>
+                    <Text weight={350}>{data.description}</Text>
+                        <Text weight={200}>Data stworzenia: {data.createdAt} </Text>
+                        <Text weight={200}>Data edytowania: {data.updatedAt} </Text>
+                </Paper>
+                    
+
+            }
+        </>
+    )
 }
-
-export default ItemInfo;
